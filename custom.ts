b8bf58a82bcdd75138a6a8720bd860a29c94caf3
garybarnes19660630 <gary.barnes@exemail.com.au>
2019-09-14 01:16:19 +1000

    Added in the code for spiWriteProc.

--- custom.ts
+++ custom.ts
@@ -3,6 +3,7 @@ namespace pins {
 
     //% blockId="SPI_WRITE_PROCEDURE" block="spi write %x"
     export function spiWriteProc(x: number) {
+        let placebo = 0
+        placebo = pins.spiWrite(0)
     }
-
 }
