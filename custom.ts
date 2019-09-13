2d1740b1192db0d26361bb6bb09014800f16adcd
garybarnes19660630 <gary.barnes@exemail.com.au>
2019-09-13 22:45:30 +1000

    Initial revision

---
+++ custom.ts
@@ -0,0 +1,8 @@
+// Add SPI functionality not available in the default code.
+namespace Pins {
+
+    //% blockId="SPI_WRITE_PROCEDURE" block="spi write%x"
+    export function spiWrite(x: string) {
+    }
+
+}
